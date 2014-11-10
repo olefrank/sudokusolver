3481bbe7649b8b763b4e92049ae1ebf8615a2b89
Ole Frank Jensen <olefrankjensen@gmail.com>
2014-11-10 23:42:10 +0100

    button states (start, reset)

--- js/game.controller.js
+++ js/game.controller.js
@@ -6,60 +6,73 @@ angular
     .module("app")
     .controller("gameCtrl", ["$scope", "solutionSvc", gameCtrl]);
 
-
 function gameCtrl($scope, solutionSvc) {
 
-	$scope.isClicked = false;
+    $scope.solution = solutionSvc.getSolution();
+	$scope.isSolving = false;
 	$scope.numSteps = 0;
 
-	// sudoku grid
-	$scope.solution = solutionSvc.getSolution();
+    var worker;
+    var stateIndex = 0;
+    $scope.btnStates = [
+        { text: "Start" },
+        { text: "Reset" },
+    ];
+    $scope.currentState = $scope.btnStates[stateIndex];
 
-	// update solution grid
-	var updateSolution = function(row, col, num) {
-		$scope.solution[row][col].value = num;
-		$scope.$apply();
-	}
+    $scope.clickHandler = function() {
+        // handle state
+        switch ( $scope.currentState.text.toLowerCase() ) {
+            case "start" :
+                startSolving();
+                break;
+            case "reset" :
+                reset();
+                break;
+        }
 
-	// reference to worker
-	var worker;
-	
-	// button click -> start worker thread
-	$scope.clickHandler = function() {
-		if ($scope.isClicked) {
-			$scope.isClicked = false;
+        // switch to next state
+        stateIndex = (stateIndex + 1) % $scope.btnStates.length;
+        $scope.currentState = $scope.btnStates[stateIndex];
+        console.log( (stateIndex + 1) % $scope.btnStates.length);
 
-			worker.terminate();
-		}
-		else {
-			$scope.isClicked = true;
+    }
 
-			worker = new Worker('js/worker.js');
-			worker.addEventListener('message', function(e) {
+    var startSolving = function() {
+        $scope.isSolving = true;
+        worker = new Worker('js/worker.js');
+        worker.addEventListener('message', $scope.workerHandler, false);
 
-				if (e.data === "finished") {
-					$scope.isClicked = false;
-                    console.log($scope.isClicked);
-                    $scope.$apply();
-				}
-				else {
-					var row = e.data.row;
-					var col = e.data.col;
-					var num = e.data.num;
+        // start worker with messate
+        var o = {
+            grid: $scope.solution,
+            row: 0,
+            col: 0
+        };
+        worker.postMessage(o);
+    };
 
-					updateSolution(row, col, num);
-					$scope.numSteps++;
-				}
-			}, false);
-			
-			// send message to worker
-			var o = {
-				grid: $scope.solution,
-				row: 0,
-				col: 0
-			};
-			worker.postMessage(o);
-		}
-	}
+    var reset = function() {
+        $scope.solution = solutionSvc.getSolution();
+        $scope.numSteps = 0;
+    };
+
+    $scope.workerHandler = function(e) {
+        if (e.data === "finished") {
+            $scope.isSolving = false;
+        }
+        else {
+            var row = e.data.row;
+            var col = e.data.col;
+            var num = e.data.num;
+            updateGrid(row, col, num);
+        }
+        $scope.$apply();
+    };
+
+    var updateGrid = function(row, col, num) {
+        $scope.solution[row][col].value = num;
+        $scope.numSteps++;
+    };
 
 }
